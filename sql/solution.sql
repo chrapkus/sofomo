215f84bbd75393677ebd3ed4b3617ee41abb8822
szymon_grzebyta <chrapkus@gmail.com>
2023-11-20 15:52:33 +0100

    rename file

--- sql/solution.sql
+++ sql/solution.sql
@@ -1,51 +1,38 @@
--- Create Table A and insert values
-DROP TABLE IF EXISTS table_a
-;
-CREATE TABLE IF NOT EXISTS table_a (
-    dimension_1 varchar,
-    dimension_2 varchar,
-    dimension_3 varchar,
-    measure_1 int
-)
-;
-INSERT INTO table_a (dimension_1, dimension_2, dimension_3, measure_1)
-VALUES
-    ('a', 'I', 'K', 1),
-    ('a', 'J', 'L', 7),
-    ('b', 'I', 'M', 2),
-    ('c', 'J', 'N', 5)
-;
-
--- Create Table B and insert values
-DROP TABLE IF EXISTS table_b
-;
-CREATE TABLE IF NOT EXISTS table_b (
-    dimension_1 varchar,
-    dimension_2 varchar,
-    measure_2 int
-)
-;
-INSERT INTO table_b (dimension_1, dimension_2, measure_2)
-VALUES
-    ('a', 'J', 7),
-    ('b', 'J', 10),
-    ('d', 'J', 4)
-;
-
--- Create Table MAP and insert values
-DROP TABLE IF EXISTS table_map
-;
-CREATE TABLE IF NOT EXISTS table_map (
-    dimension_1 varchar,
-    correct_dimension_2 varchar
-)
-;
-INSERT INTO table_map (dimension_1, correct_dimension_2)
-VALUES
-    ('a', 'W'),
-    ('a', 'W'),
-    ('b', 'X'),
-    ('c', 'Y'),
-    ('b', 'X'),
-    ('d', 'Z')
-;
+with
+    -- I assume that map table should not have any duplicates !
+    fixed_table_map as (select distinct * from table_map),
+    fixed_table_a as (
+        select
+            table_a.dimension_1,
+            fixed_table_map.correct_dimension_2 as dimension_2,
+            table_a.dimension_3,
+            table_a.measure_1
+        from table_a
+        left join fixed_table_map on table_a.dimension_1 = fixed_table_map.dimension_1
+    ),
+    fixed_table_b as (
+        select
+            table_b.dimension_1,
+            fixed_table_map.correct_dimension_2 as dimension_2,
+            table_b.measure_2
+        from table_b
+        left join fixed_table_map on table_b.dimension_1 = fixed_table_map.dimension_1
+    ),
+    joined_tables as (
+        select
+            nvl(fixed_table_a.dimension_1, fixed_table_b.dimension_1) as dimension_1,
+            nvl(fixed_table_a.dimension_2, fixed_table_b.dimension_2) as dimension_2,
+            nvl(fixed_table_a.measure_1, 0) as measure_1,
+            nvl(fixed_table_b.measure_2, 0) as measure_2
+        from fixed_table_a
+        full outer join
+            fixed_table_b on fixed_table_a.dimension_1 = fixed_table_b.dimension_1
+    )
+select
+    dimension_1,
+    dimension_2,
+    sum(measure_1) as sum_measure_1,
+    sum(measure_2) as sum_measure_2
+from joined_tables
+group by dimension_1, dimension_2
+order by dimension_1, dimension_2, sum_measure_1, sum_measure_2
